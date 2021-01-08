36bb14fd8afb4ca629f29b02ab514d035b876a9c
Stan Rock <srock@opentext.com>
2021-01-08 17:36:35 +0000

    latest try 3

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 module "gke1" {
-  source  = "tfe.srock-2741611427.workshops.arctiq.ca/arctiq/gke/google"
+  source  = "tfe.srock.2741611427.workshops.arctiq.ca/opentext/gke/google"
   version = "1.1.0"
-
-  # insert required variables here
+ 
   cluster_name = var.cluster_name
   project_id=var.gcp_project_id
-
 }
+
